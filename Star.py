a0ba594b38237905cac380e13f701ded020af62f
Rp-cloud-droid <rishikapatil15@gmail.com>
2025-06-16 17:39:28 +1000

    Add files via upload

--- Star.py
+++ Star.py
@@ -1,5 +1,28 @@
-n = int(input("Enter the number of rows: "))
-for i in range(1, n+1):
-    for i in range(i):
-        print('*', end=' ')
-    print()
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+board = turtle.Turtle()
+
+board.forward(100) # draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()

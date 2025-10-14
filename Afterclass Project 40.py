60d3d5e4b1eafc9f66276f3153da501c7e6f9e94
Rp-cloud-droid <rishikapatil15@gmail.com>
2025-10-14 18:02:25 +1100

    Update Afterclass Project 40.py

--- Afterclass Project 40.py
+++ Afterclass Project 40.py
@@ -1,39 +1,47 @@
 import sqlite3
+import pandas as pd
 
-conn = sqlite3.connect("afterclass.db")
-
+database = "cricket_database.sqlite"
+conn = sqlite3.connect(database)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS Suspects_List (
-    suspect_no INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    gender TEXT CHECK(gender IN('Male', 'Female', 'Other')),
-    age INTEGER CHECK(age > 0)
+CREATE TABLE IF NOT EXISTS Cricket_Match (
+    Match_Id INTEGER,
+    Player_Id INTEGER,
+    Runs INTEGER,
+    Wickets INTEGER,
+    Catches INTEGER
 );
 """)
+print("Table 'Cricket_Match' created successfully!")
 
-print("Database and table created successfully!")
-
-suspects = [
-    (1, 'Ayaan Kumar', 'Male', 21),
-    (2, 'Nick Nair', 'Male', 16),
-    (3, 'Anushka Raj', 'Female', 18,),
-    (4, 'Ari Thomas', 'Female', 15,),
-]
-
-cursor.executemany("INSERT OR IGNORE INTO Suspects_List VALUES (?, ?, ?, ?)", suspects)
+cursor.executemany("""
+INSERT INTO Cricket_Match (Match_Id, Player_Id, Runs, Wickets, catches)
+                   VALUES (?, ?, ?, ?, ?)
+""", [
+    (1, 101, 45, 1, 0),
+    (1, None, 70, 0, 1),
+    (2, 101, None, 2, 1),
+    (2, 103, 55, 0, None),
+    (3, 104, 60, 3, 2),
+    (3, 105, None, None, None)
+])
 conn.commit()
+print("Sample data inserted successfully!")
 
-cursor.execute("SELECT * FROM Suspects_List")
-rows = cursor.fetchall()
+cricket_match = pd.read_sql("SELECT * FROM Cricket_Match", conn)
 
-print("\nSuspect Details in Suspects List:")
-print("Suspect_No | Name | Gender | Age")
-print("---------------------------------")
-for row in rows:
-    print(row)
+print("\nFirst 5 rows of Cricket_Match table:")
+print(cricket_match.head())
 
+print("\nChecking for NULL (missing) values:")
+null_summary = cricket_match.isnull().sum()
+
+print(null_summary)
+
+print("\nColumns containing NULL values:")
+print(null_summary[null_summary > 0])
 
 conn.close()
-print("\nConnection closed successfully!")
+print("\nDatabase connection closed successfully!")

f6abc4d86fc4d4dd212fc7288351b27c6ae93089
Rp-cloud-droid <rishikapatil15@gmail.com>
2025-11-27 17:11:56 +1100

    Add files via upload

---
+++ 53 Activity 2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+HouseDF=pd.read_csv('USA_Housing.csv')
+HouseDF.head()
+HouseDF.info()
+HouseDF.describe()
+HouseDF.columns
+sns.pairplot(HouseDF)
+numeric_df = HouseDF.select_dtypes(include=['int64', 'float64'])
+
+sns.heatmap(numeric_df.corr(), annot=True, cmap='coolwarm')
